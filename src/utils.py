ea7139588a60b3538135ff6b8fb0a7ff26073391
Francisco Almeida <franciscocfalmeida@gmail.com>
2020-03-01 15:09:46 +0000

    implement function which reads a level file and returns a Bubble matrix

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ from src.bubble_types import BubbleTypes
 
 # Creates a board with the specified height and width (cells)
 # and fills it with empty bubbles
+# Returns a matrix instead of a Board object
 def create_empty_board(height, width):
   board = []
 
@@ -16,9 +17,27 @@ def create_empty_board(height, width):
 
 # Prints the board on the console
 def print_board(board):
-  for i in range(len(board)):
+  for i in range(len(board.matrix)):
     print('\n')
-    for j in range(len(board[i])):
-      print('[', board[i][j], ']', end='')
+    for j in range(len(board.matrix[i])):
+      print('[', board.matrix[i][j], ']', end='')
 
   print('\n')
+
+# Reads the given level file and returns a matrix representation of the board
+def read_level_file(file):
+  board = []
+
+  f = open(file, 'r')
+  lines = f.readlines()
+
+  for line in lines:
+    lineArr = line.split()
+    aux = []
+
+    for num in lineArr:
+      aux.append(Bubble(BubbleTypes(int(num))))
+    
+    board.append(aux)
+
+  return board

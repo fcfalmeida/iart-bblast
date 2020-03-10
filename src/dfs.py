8b946a119ff2695f8cb70be220935b4cd89c834d
Francisco Almeida <franciscocfalmeida@gmail.com>
2020-03-10 17:44:28 +0000

    implement dfs algorithm

---
+++ src/dfs.py
@@ -0,0 +1,24 @@
+from src.search_algorithm import SearchAlgorithm
+from src.game_results import GameResults
+import src.utils as utils
+
+class DFS(SearchAlgorithm):
+  @staticmethod
+  def execute(game_state):
+    stack = utils.get_possible_next_states_path(game_state)
+    max_touches = game_state.touches_left
+
+    while stack:
+      solution = stack.pop()
+      state, move = solution[-1]
+
+      if state.result == GameResults.Win: return utils.extract_solution(solution)
+
+      next_possible_states = utils.get_possible_next_states(state)
+
+      for next_state in next_possible_states:
+        new_solution = list(solution)
+        new_solution.append(next_state)
+        stack.append(new_solution)
+       
+    return []

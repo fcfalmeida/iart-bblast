733e33a01f6057c3cf3953e8b4e2c9135536d03f
Miguel Castro <1160574@isep.ipp.pt>
2020-03-23 19:09:51 +0000

    Fixed algorithm

--- src/greedy_algorithm.py
+++ src/greedy_algorithm.py
@@ -1,39 +1,39 @@
 from src.search_algorithm import SearchAlgorithm
 from src.game_results import GameResults
 import src.utils as utils
-import sys
 
 class Greedy(SearchAlgorithm):
   @staticmethod
   def execute(game_state, heuristic):
-    sys.setrecursionlimit(10**6) 
-
-    solution = Greedy.recursive(game_state, [], [], [], heuristic)
+    #solution = Greedy.recursive(game_state, [], [], [], heuristic)
+    solution = Greedy.algorithm(game_state, heuristic)
 
     return utils.extract_solution(solution)
 
   @staticmethod
-  def recursive(game_state, prev_state, visited_state, solution, heuristic):
-    next_possible_states = utils.get_possible_next_states_path(game_state)
-    optimal_value = -1000
-    optimal_state = None
+  def algorithm(game_state, heuristic):
+    stack = utils.get_possible_next_states_path(game_state)
+    state = [(game_state, None)]
+    solution = []
+    visited_states = []
 
-    if game_state.result == GameResults.Win: return []
+    while(state[-1][0].result != GameResults.Win):
+      optimal_value = -1000
+      optimal_state = None
 
-    for next_state in next_possible_states:
-      heuristic_value = heuristic.calculate(next_state[0][0])
-      if heuristic_value > optimal_value and (next_state[0][0].board not in visited_state):
-        optimal_state = next_state[0]
-        optimal_value = heuristic_value
-    
-    if optimal_state != None:
-      prev_state.append(game_state)
-      solution.append(optimal_state)
-      Greedy.recursive(optimal_state[0], prev_state, visited_state, solution, heuristic)
-    else:
-      visited_state.append(game_state.board)
-      prev = prev_state.pop()
-      solution.pop()
-      Greedy.recursive(prev, prev_state, visited_state, solution, heuristic)
+      for next_state in stack:
+        heuristic_value = heuristic.calculate(next_state[0][0])
+        if heuristic_value > optimal_value and (next_state[0][0].board.matrix not in visited_states):
+          optimal_state = next_state[0]
+          optimal_value = heuristic_value
 
-    return solution
+      if optimal_state != None:
+        stack = utils.get_possible_next_states_path(optimal_state[0])
+        state.append(optimal_state)
+      else:
+        visited_states.append(state[-1][0].board.matrix)
+        state.pop()
+        stack = utils.get_possible_next_states_path(state[-1][0])
+
+    state.pop(0)
+    return state

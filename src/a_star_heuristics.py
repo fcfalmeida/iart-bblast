db6c739dd132521e2dbd376ced8c8104bb71e551
Miguel Castro <1160574@isep.ipp.pt>
2020-03-26 15:48:34 +0000

    A* Heuristics

---
+++ src/a_star_heuristics.py
@@ -0,0 +1,7 @@
+from src.score_heuristic import Score
+from src.bubbles_left_heuristic import BubblesLeft
+
+aStarHeuristics = {
+    1: ('Abc', Score),
+    2: ('Dfg', BubblesLeft),
+}
--- src/main.py
+++ src/main.py
@@ -7,144 +7,169 @@ from src.game_state import GameState
 from src.game_results import GameResults
 from src.algorithms import algorithms
 from src.heuristics import heuristics
+from src.a_star_heuristics import aStarHeuristics
+
 
 def read_op():
-  op = input('Select an option: ')
-  print()
-  return int(op)
+    op = input("Select an option: ")
+    print()
+    return int(op)
+
 
 def print_header(game_state):
-    print('=' * 37)
-    print('Touches left: ', game_state.touches_left, ' | Score: ', game_state.score)
-    print('=' * 37)
+    print("=" * 37)
+    print("Touches left: ", game_state.touches_left, " | Score: ",
+          game_state.score)
+    print("=" * 37)
     print()
+
 
 def game_loop(game_state):
-  while game_state.result == None:
+    while game_state.result == None:
+        print_header(game_state)
+        utils.print_board(game_state.board)
+
+        player_input = input("Touch row,col: ").split(",")
+        row = int(player_input[0])
+        col = int(player_input[1])
+
+        print()
+
+        game_state.update_board(row, col)
+
     print_header(game_state)
     utils.print_board(game_state.board)
-    
-    player_input = input('Touch row,col: ').split(',')
-    row = int(player_input[0])
-    col = int(player_input[1])
-    
+
+    if game_state.result == GameResults.Win:
+        print("**** You Won! ****")
+    else:
+        print("**** You Lost! ****")
+
     print()
 
-    game_state.update_board(row, col)
-
-  print_header(game_state)
-  utils.print_board(game_state.board)
-
-  if game_state.result == GameResults.Win:
-    print('**** You Won! ****')
-  else:
-    print('**** You Lost! ****')
-
-  print()
 
 def levels_menu():
-  levels = utils.list_levels()
+    levels = utils.list_levels()
 
-  print('**** Level ****')
+    print("**** Level ****")
 
-  for level in levels:
-    print(level, ' - Level ', level, sep='')
+    for level in levels:
+        print(level, " - Level ", level, sep="")
 
-  op = read_op()
-  selected_level = levels[op]
+    op = read_op()
+    selected_level = levels[op]
 
-  return selected_level
+    return selected_level
+
 
 def algorithms_menu():
-  print('**** Algorithm ****')
+    print("**** Algorithm ****")
 
-  for alg_key in algorithms:
-    algorithm = algorithms[alg_key]
-    print(alg_key, ' - ', algorithm[0])
+    for alg_key in algorithms:
+        algorithm = algorithms[alg_key]
+        print(alg_key, " - ", algorithm[0])
 
-  heuristic = None
-  op = read_op()
-  if op == 5:
-    heuristic = heuristic_menu()
-  selected_algorithm = algorithms[op][1]
+    heuristic = None
+    op = read_op()
+    if op == 5:
+        heuristic = heuristic_menu()
+    if op == 6:
+        heuristic = a_star_heuristic_menu()
+    selected_algorithm = algorithms[op][1]
 
-  return selected_algorithm, heuristic
+    return selected_algorithm, heuristic
+
 
 def play():
-  selected_level = levels_menu()
+    selected_level = levels_menu()
 
-  board_matrix, max_touches = utils.read_level_file(selected_level)
-  board = Board(board_matrix)
+    board_matrix, max_touches = utils.read_level_file(selected_level)
+    board = Board(board_matrix)
 
-  game_state = GameState(board, max_touches)
+    game_state = GameState(board, max_touches)
 
-  game_loop(game_state)
+    game_loop(game_state)
+
 
 def solve():
-  selected_level = levels_menu()
-  algorithm, heuristic = algorithms_menu()
+    selected_level = levels_menu()
+    algorithm, heuristic = algorithms_menu()
 
+    board_matrix, max_touches = utils.read_level_file(selected_level)
+    board = Board(board_matrix)
 
-  board_matrix, max_touches = utils.read_level_file(selected_level)
-  board = Board(board_matrix)
+    game_state = GameState(board, max_touches)
 
-  game_state = GameState(board, max_touches)
-
-  print_header(game_state)
-  utils.print_board(game_state.board)
-
-  start_time = time.time()
-  solution = algorithm.execute(game_state, heuristic)
-  end_time = time.time()
-  total_time = round(end_time - start_time, 2)
-
-  for move in solution:
-    move_row, move_col = move
-    game_state.update_board(move_row, move_col)
-    print('Move: [', move_row, ',', move_col, ']')
     print_header(game_state)
     utils.print_board(game_state.board)
 
-  if game_state.result == GameResults.Win:
-    print('**** You Won! ****')
-  else:
-    print('**** You Lost! ****')
+    start_time = time.time()
+    solution = algorithm.execute(game_state, heuristic)
+    end_time = time.time()
+    total_time = round(end_time - start_time, 2)
 
-  print('\nSolved in ', total_time, ' seconds', sep='')
-  print()
+    for move in solution:
+        move_row, move_col = move
+        game_state.update_board(move_row, move_col)
+        print("Move: [", move_row, ",", move_col, "]")
+        print_header(game_state)
+        utils.print_board(game_state.board)
+
+    if game_state.result == GameResults.Win:
+        print("**** You Won! ****")
+    else:
+        print("**** You Lost! ****")
+
+    print("\nSolved in ", total_time, " seconds", sep="")
+    print()
+
 
 def print_options(options_dict):
-  for op in options_dict:
-    option = options_dict.get(op)
-    print(option[0])
+    for op in options_dict:
+        option = options_dict.get(op)
+        print(option[0])
+
 
 def main_menu():
-  options = {
-    1: ['1 - Play', play],
-    2: ['2 - Solve', solve]
-  }
+    options = {1: ["1 - Play", play], 2: ["2 - Solve", solve]}
 
-  print_options(options)
+    print_options(options)
 
-  op = read_op()
+    op = read_op()
 
-  func = options.get(op, lambda: 'Invalid Option')[1]
-  return func()
+    func = options.get(op, lambda: "Invalid Option")[1]
+    return func()
+
 
 def main_loop():
-  while True:
-    main_menu()
+    while True:
+        main_menu()
+
 
 def heuristic_menu():
-  print("**** Heuristic ****")
+    print("**** Heuristic ****")
 
-  for heur_key in heuristics:
-    heuristic = heuristics[heur_key]
-    print(heur_key, ' - ', heuristic[0])
+    for heur_key in heuristics:
+        heuristic = heuristics[heur_key]
+        print(heur_key, " - ", heuristic[0])
 
-  op = read_op()
-  selected_heuristic = heuristics[op][1]
+    op = read_op()
+    selected_heuristic = heuristics[op][1]
 
-  return selected_heuristic
+    return selected_heuristic
+
+
+def a_star_heuristic_menu():
+    print("**** Heuristic ****")
+
+    for heur_key in aStarHeuristics:
+        heuristic = aStarHeuristics[heur_key]
+        print(heur_key, " - ", heuristic[0])
+
+    op = read_op()
+    selected_heuristic = aStarHeuristics[op][1]
+
+    return selected_heuristic
+
 
 main_loop()
